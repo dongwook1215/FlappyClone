58aae6fec3baef142c1dda9c2812f73d5c8b09d9
김동욱 <kdo6338@gmail.com>
2022-01-29 00:44:05 +0900

    FlappyBird Clone Codding

--- Assets/BirdJump.cs
+++ Assets/BirdJump.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BirdJump : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float jumpPower;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Start 입니다.");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Update 입니다.");
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 속도
+            rb.velocity = Vector2.up * jumpPower; //(0,1)
+        }
     }
 }
